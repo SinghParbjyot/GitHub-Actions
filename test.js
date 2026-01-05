a5214aba20af715e83fe8cabf4da200b455a3e25
Singh005__ <Parksingh005@gmail.com>
2026-01-05 16:18:40 +0100

    Add test for sum of 2 and 2
    Prueba sencilla para probar nuestro github-actions

---
+++ test.js
@@ -0,0 +1,10 @@
+// Prueba de funcionamiento
+const suma = 2 + 2;
+
+if (suma === 4) {
+  console.log("Test Pasado: La suma de 2 + 2 es 4.");
+  process.exit(0); // Código 0 significa exito
+} else {
+  console.error(" Test Fallido: La suma no dio 4.");
+  process.exit(1); // Código 1 significa error
+}
